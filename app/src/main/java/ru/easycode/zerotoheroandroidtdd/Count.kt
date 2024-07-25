eff8b534daf5355f912ef3af0c3cf85b169b6edc
andrew <andrew.maneshin@mail.ru>
2024-07-25 18:18:47 +0300

    Task012Test increment-max resolved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -2,17 +2,25 @@ package ru.easycode.zerotoheroandroidtdd
 
 interface Count {
 
-    fun increment(number: String): String
+    fun increment(number: String): UiState
 
-    class Base(private val step: Int) : Count {
+    class Base(private val step: Int, private val max: Int) : Count {
 
         init {
             if (step <= 0)
                 throw IllegalStateException("step should be positive, but was $step")
+            else if (max <= 0)
+                throw IllegalStateException("max should be positive, but was $max")
+            else if (step > max)
+                throw IllegalStateException("max should be more than step")
         }
 
-        override fun increment(number: String): String {
-            return (number.toInt() + step).toString()
+        override fun increment(number: String): UiState {
+            val increment = number.toInt() + step
+            return if (increment + step > max)
+                UiState.Max(increment.toString())
+            else
+                UiState.Base(increment.toString())
         }
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,16 +8,41 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private var state: UiState = UiState.Base("0")
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.incrementButton)
-        val textView = findViewById<TextView>(R.id.countTextView)
-        val count = Count.Base(2)
+        button = findViewById(R.id.incrementButton)
+        textView = findViewById(R.id.countTextView)
+        val count = Count.Base(2, 4)
 
         button.setOnClickListener {
-            textView.text = count.increment(textView.text.toString())
+            state = count.increment(textView.text.toString())
+            state.apply(textView, button)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(KEY, state)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        state = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(KEY, UiState::class.java) as UiState
+        } else {
+            savedInstanceState.getSerializable(KEY) as UiState
+        }
+
+        state.apply(textView, button)
+    }
+
+    companion object {
+        private const val KEY = "key"
     }
 }
